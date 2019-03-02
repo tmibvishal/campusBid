d02c949c2dd4f046883f9c8fefd6592fb1f5dc26
Vishal Singh <tmibvishal@gmail.com>
2019-03-03 01:44:20 +0530

    shift database related operations like connect function to a new module

--- app.js
+++ app.js
@@ -2,12 +2,21 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+
 //initiating the express app
 const app = express();
 
+//making the connection to the database
+const database = require("./database/database");
+database.connect();
+
 // Imports routes for the products
 const router = require("./routes/product.route");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}))
 app.use("/products", router)
+
 //dedicating a port number and listening to that port
 let port = 5000;
 app.listen(port, function(){
---
+++ database/database.js
@@ -0,0 +1,12 @@
+function connect(){
+    const db_url = `mongodb+srv://${hiddenFromGit_ImpInfo.username}:${hiddenFromGit_ImpInfo.password}@cluster0-bxpre.mongodb.net/test?retryWrites=true`;
+    let mongoDB = process.env.MONGODB_URI || db_url;
+    mongoose.connect(mongoDB, {useNewUrlParser: true});
+    mongoose.Promise = global.Promise;
+    let db = mongoose.connection;
+    db.on("error", console.error.bind(console, 'MongoDB connection error:'))
+}
+
+module.exports = {
+    connect
+}
