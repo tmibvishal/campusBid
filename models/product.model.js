bf9b0959c24b5977c0f3663be13643e44e7f715f
Vishal Singh <tmibvishal@gmail.com>
2019-03-03 01:07:15 +0530

    make the connection to the database

--- models/product.model.js
+++ models/product.model.js
@@ -1,5 +1,13 @@
 //M part: Model
 const mongoose = require("mongoose");
+const hiddenFromGit_ImpInfo = require("../hiddenFromGit_ImpInfo");
+const db_url = `mongodb+srv://${hiddenFromGit_ImpInfo.username}:${hiddenFromGit_ImpInfo.password}@cluster0-bxpre.mongodb.net/test?retryWrites=true`;
+let mongoDB = process.env.MONGODB_URI || db_url;
+mongoose.connect(mongoDB, { useNewUrlParser: true });
+mongoose.Promise = global.Promise;
+let db = mongoose.connection;
+db.on("error", console.error.bind(console, 'MongoDB connection error:'))
+
 const Schema = mongoose.Schema;
 
 let productSchema = Schema({
